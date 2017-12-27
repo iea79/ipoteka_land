c50399270f0dfdeb878ca0726f03999c0651f111
Evgeny Ivanov <busforward@gmail.com>
2017-12-27 18:55:10 +0500

    форма

--- send.php
+++ send.php
@@ -16,8 +16,8 @@ $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
 
 // Дополнительные заголовки
-$headers .= 'To: workrealcapital@yandex.ru' . "\r\n"; // Свое имя и phone
-$headers .= 'From: Письмо с сайта>' . "\r\n";
+// $headers .= 'To: workrealcapital@yandex.ru' . "\r\n"; // Свое имя и phone
+// $headers .= 'From: Письмо с сайта' . "\r\n";
 
 
 // Отправляем
