fca74a027ec096fbad22fff8644bcf9352f2163c
Evgeny Ivanov <busforward@gmail.com>
2017-12-27 17:54:01 +0500

    Доработка + форма

--- js/function.js
+++ js/function.js
@@ -105,6 +105,18 @@ $(document).ready(function() {
 
 	calculate();
 
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "POST", //Метод отправки
+            url: "/send.php", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                //код в этом блоке выполняется при успешной отправке сообщения
+                $('#rezult').html('Ваше сообщение отпрвлено!');
+            }
+        })
+    });
 
 });
 
